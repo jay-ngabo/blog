72a6bab4c59e0af544ea6945178868bbd31121af
jacqueline ngabo <ngabo.jay@gmail.com>
2022-05-19 21:08:53 +0300

    add all changes

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     BASE_URL='http://quotes.stormconsultancy.co.uk/random.json'
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blogs'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://geejay:jayngabo@localhost/blogs'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
