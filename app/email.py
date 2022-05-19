0fefa47fd38d4dce37ebef2f5efca3d0f0bc24c5
jacqueline ngabo <ngabo.jay@gmail.com>
2022-05-19 21:11:35 +0300

    add all changes

--- app/email.py
+++ app/email.py
@@ -4,7 +4,7 @@ from . import mail
 
 
 def mail_message(subject,template,to,**kwargs):
-    sender_email = 'kicaleb13@gmail.com'
+    sender_email = 'ngabo.jay@gmail.com'
 
     email = Message(subject, sender=sender_email, recipients=[to])
     email.body= render_template(template + ".txt",**kwargs)
